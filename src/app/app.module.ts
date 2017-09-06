caed03ce62aa02c00b028c9ec6b8a599ba2af5cc
Stephen Corry <stevemcorry@gmail.com>
2017-09-06 14:55:35 -0600

    Add Angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//IMPORTS
+
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+      apiKey: "AIzaSyBQpo1-XXUaxbkWHFrVRf_GYvXl8uPGCG0",
+      authDomain: "crowd-control-bd4b8.firebaseapp.com",
+      databaseURL: "https://crowd-control-bd4b8.firebaseio.com",
+      projectId: "crowd-control-bd4b8",
+      storageBucket: "crowd-control-bd4b8.appspot.com",
+      messagingSenderId: "678842753301"
+    };
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +28,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
